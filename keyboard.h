e7406fa7271139322d0de39961bd049007baeab4
KY <162005150+Atar369@users.noreply.github.com>
2024-04-22 00:52:50 +0800

    Update keyboard.h

--- keyboard.h
+++ keyboard.h
@@ -1,13 +1,28 @@
 #pragma once
 
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
 #define KEY_UP 650
 #define KEY_DOWN 660
-#define KEY_RIGHT 670
 #define KEY_LEFT 680
+#define KEY_RIGHT 670
 #define KEY_ENTER 10 // '\n' key
 #define KEY_ESC 27  
 #define KEY_SPACE 32
 #define KEY_ACTION 69
 #define KEY_EXIT 81
 
-void get_userInput();
+class Keyboard {
+    public:
+    int key;
+
+    public:
+    bool key_pressed(int key);
+    bool waiting(int key);
+    void get_userInput();
+    //void stop();
+};
+
