33ce18768a0a63d2673981adb3f84bf517111229
KY <162005150+Atar369@users.noreply.github.com>
2024-04-22 00:45:48 +0800

    Update player.h

--- player.h
+++ player.h
@@ -1,8 +1,23 @@
 #pragma once
 
+#include <iostream>
+#include <unistd.h>
 #include <string>
+#include <cstring>
+#include "keyboard.h"
+#include "constant.h"
+
 
 using namespace std;
+
+struct object {
+    int hrz, vtrl;
+    int x, y;
+    int direction;
+    string symbol;
+};
+
+extern object player;
 
 void moveUp();
 
@@ -14,6 +29,6 @@ void moveRight();
 
 void stopMovement();
 
-void player_move(string key, short current_map[ROWS][COLS]);
+void player_move(int key, short current_map[ROWS][COLS]);
 
-
+void player_collision(short current_lvl[][COLS]);
