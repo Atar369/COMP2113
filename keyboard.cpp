4cceabe351bec67aa90fdc5a090acd165901b4a3
KY <162005150+Atar369@users.noreply.github.com>
2024-04-22 00:53:12 +0800

    Update keyboard.cpp

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,171 +1,61 @@
-#include <iostream>
-#include <termios.h>
-#include <unistd.h>
-#include <string>
-#include <vector>
-using namespace std;
+#include "keyboard.h"
 
-const int COLS = 27;
-const int ROWS = 20;
-
-struct Player {
-    int x;
-    int y;
-    int hrz;
-    int vtcl;
-    string symbol;
-};
-
-Player player;
-
-void moveUp() {
-    player.vtcl = -1;
-}
-
-void moveDown() {
-    player.vtcl = 1;
-}
-
-void moveLeft() {
-    player.hrz = -1;
-}
-
-void moveRight() {
-    player.hrz = 1;
-}
-
-void stopMovement() {
-    player.hrz = 0;
-    player.vtcl = 0;
-}
-
-int dir_x;
-int dir_y;
-
-void player_move(string key) {
-    // Key check
-    int key_left  = ( key == "LEFT" ) ? 1 : 0;
-    int key_right = ( key == "RIGHT" ) ? 1 : 0;
-    int key_down  = ( key == "DOWN" ) ? 1 : 0;
-    int key_up    = ( key == "UP" ) ? 1 : 0;
-
-    // Reset player's movement
-    stopMovement();
-
-    // Update player's movement based on key input
-    if (key_left) {
-        moveLeft();
+bool Keyboard::key_pressed(int key) {
+    if (key != 0 && (key == KEY_UP || key == KEY_DOWN || key == KEY_LEFT || key == KEY_RIGHT || key == KEY_ENTER || key == KEY_ESC || key == KEY_SPACE || key == KEY_ACTION || key == KEY_EXIT) ) {
+        return 0;
     }
-    if (key_right) {
-        moveRight();
-    }
-    if (key_up) {
-        moveUp();
-    }
-    if (key_down) {
-        moveDown();
-    }
-
-    // Animation and direction shoot
-    if (player.hrz == 0 && player.vtcl == 0) {
-        player.symbol = "|@|";
-    } 
-    else {
-        if (player.hrz == 1) {
-            player.symbol = "|@>";
-        }
-        if (player.hrz == -1) {
-            player.symbol = "<@|";
-        }
-        if (player.vtcl == -1) {
-            player.symbol = "/@\\";
-        }
-        if (player.vtcl == 1) {
-            player.symbol = "\\@/";
-        }
-    }
-
-    player.x += player.hrz;
-    player.y += player.vtcl;
+    return 1;
 }
 
-void displayBoard(const vector<vector<string> >& board) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            if (i == player.y && j == player.x) {
-                cout << player.symbol << " ";
-            } else {
-                cout << board[i][j] << " ";
-            }
-        }
-        cout << endl;
+bool Keyboard::waiting(int key) {
+    if (key == KEY_ENTER) {
+        return 0;
     }
+    return 1;
 }
 
-int main() {
-    string key;
-    struct termios oldSettings, newSettings;    // termios is a struct that contains terminal attributes
-    tcgetattr(STDIN_FILENO, &oldSettings);  // write the current terminal attributes into oldSettings
-    newSettings = oldSettings; 
-    newSettings.c_lflag &= ~(ICANON | ECHO);    // Disable canonical mode (buffered i/o) and local echo to read the input character by character
-    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);     // int, action (immediate adjustment), settings
+void Keyboard::get_userInput() {
+    struct termios oldSettings, newSettings;
+    tcgetattr(STDIN_FILENO, &oldSettings);
+    newSettings = oldSettings;
+    newSettings.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+    newSettings.c_cc[VTIME] = 0;
+    newSettings.c_cc[VMIN] = 1;
+    char userInput = getchar();
 
-    player.x = 0;
-    player.y = 0;
-
-    vector<vector<string> > board(ROWS, vector<string>(COLS, " "));
-
-    while (true) {
-        // Clear the console
-        cout << "\033[2J\033[1;1H";
-
-        // Display the board
-        displayBoard(board);
-
-        cout << "Enter a key: " << endl;
-        
-        // Debug: Print the player animation
-        // cout << "Player position: (" << player.x << ", " << player.y << ")" << endl;
-        // cout << "Player animation: " << player.symbol << endl; 
-
-        board[player.y][player.x] = " ";
-
-        char userInput = getchar(); // Get the user input by 1 char
-
-        switch (userInput) {
-        case 'W': case 'w':
-            player_move("UP");
+    switch (userInput) {
+        case 'W': case 'w': case KEY_UP:
+            key = KEY_UP;
             break;
-        case 'A': case 'a':
-            player_move("LEFT");
+        case 'A': case 'a': case KEY_LEFT:
+            key = KEY_LEFT;
             break;
-        case 'S': case 's':
-            player_move("DOWN");
-            break; 
-        case 'D': case 'd':
-            player_move("RIGHT");
-            break; 
-        case 'E': case 'e':
-            
+        case 'S': case 's': case KEY_DOWN:
+            key = KEY_DOWN;
             break;
-        
+        case 'D': case 'd': case KEY_RIGHT:
+            key = KEY_RIGHT;
+            break;
+        case KEY_ENTER:
+            key = KEY_ENTER;
+            break;
+        case KEY_ESC:
+            key = KEY_ESC;
+            break;
+        case KEY_SPACE:
+            key = KEY_SPACE;
+            break;
+        case 'E': case 'e': 
+            key = KEY_ACTION;
+            break;
         case 'Q': case 'q':
-            cout << "Exiting..." << endl;
-            return 0;
-        
-        default:
-            cout << "Invalid key pressed" << endl;
+            key = KEY_EXIT;
             break;
-        }
-
-        board[player.y][player.x] = player.symbol;
-
-        // Update the board with the player's position
-        displayBoard(board);
-        
+        //default:
+            // Ignore invalid keys
+            //break;
     }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
-    return 0;
-
 }
+
