c6d411b808ca394fe05800c337f1dc320109d9e9
KY <162005150+Atar369@users.noreply.github.com>
2024-04-23 20:34:07 +0800

    Update maintest.cpp

--- maintest/maintest.cpp
+++ maintest/maintest.cpp
@@ -2,13 +2,8 @@
 #include <sys/ioctl.h>
 #include <string>
 #include <unistd.h>
-//#include "player.h"
-//#include "draw.h"
-//#include "object.h"
 #include "menu.h"
-//#include "format.h"
-//#include "constant.h"
-#include "keyboard.h"
+#include "Hero.h"
 using namespace std;
 
 // Escape sequences for cursor positioning and movement
@@ -19,17 +14,17 @@ int main() {
     typedef enum {
         STATE_MENU,
         STATE_HTP,
+        STATE_ACHIEVE,
         STATE_GAME,
         STATE_EXIT,
     } game_states;
 
     // Get the dimensions of the terminal window
-    //struct winsize w;
-    //ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    //int rows = w.ws_row;
-    //int cols = w.ws_col;
-    int rows = 40;
-    int cols = 150;
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int rows = w.ws_row;
+    int cols = w.ws_col;
+
      // Calculate the coordinates for centering the text
     int startRow = rows / 2;
     int startCol = cols / 2; // Adjust for the length of the longest string
@@ -37,8 +32,6 @@ int main() {
     // Init current state
     game_states current_state;
     current_state = STATE_MENU;
-
-    Keyboard keyboard;
 
     ////////////////
     // Main loop
@@ -54,6 +47,11 @@ int main() {
     const string button_HTP[2] = {
         "> HOW TO PLAY <",
         "  How to Play  ",
+    };
+
+    const string button_achieve[2] = {
+        "> ACHIEVEMENTS <",
+        "  Acheivements  ",
     };
 
     const string button_exit[2] = {
@@ -76,9 +74,9 @@ int main() {
 
     while (current_state != STATE_EXIT) {
         // Clear screen
-        cout << clear_screen;
+        system("clear");
         cout << save_cursor_position; // Save cursor position
-        cout << hide_cursor;  // Save cursor position
+        //cout << hide_cursor;  
 
   /**     int check_waiting = keyboard.waiting(keyboard.key);
 
@@ -90,7 +88,7 @@ int main() {
         }
         */
 
-        int select_start_game = 1, select_HTP = 1, select_exit = 1;
+        int select_start_game = 1, select_HTP = 1, select_achieve = 1, select_exit = 1;
 
         // In menu state
         switch(current_state) {
@@ -112,8 +110,8 @@ int main() {
                         
                     if (keyboard.key == KEY_DOWN) {
                         menu_button++;
-                        if (menu_button >= 2) {
-                            menu_button = 2;
+                        if (menu_button >= 3) {
+                            menu_button = 3;
                         }
                     } 
 
@@ -121,16 +119,25 @@ int main() {
                     if (menu_button == 0) {
                         select_start_game = 0;
                         select_HTP = 1;
+                        select_achieve = 1;
                         select_exit = 1;
                     } 
                     else if (menu_button == 1) {
                         select_start_game = 1;
                         select_HTP = 0;
+                        select_achieve = 1;
                         select_exit = 1;
                     }
                     else if (menu_button == 2) {
                         select_start_game = 1;
                         select_HTP = 1;
+                        select_achieve = 0;
+                        select_exit = 1;
+                    }
+                    else if (menu_button == 3) {
+                        select_start_game = 1;
+                        select_HTP = 1;
+                        select_achieve = 1;
                         select_exit = 0;
                     }
 
@@ -139,7 +146,9 @@ int main() {
 
                     cout << default_format << startRow - logo_h_size + 11 << ";" << startCol - (button_HTP[select_HTP].length())/2 << "H" << button_HTP[select_HTP] << endl;
 
-                    cout << default_format << startRow - logo_h_size + 13 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
+                    cout << default_format << startRow - logo_h_size + 13 << ";" << startCol - (button_achieve[select_achieve].length())/2 << "H" << button_achieve[select_achieve] << endl;
+
+                    cout << default_format << startRow - logo_h_size + 15 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
                     
                     keyboard.get_userInput();
                 }
@@ -153,8 +162,11 @@ int main() {
                         current_state = STATE_HTP;
                         break;
                     case 2:
-                        current_state = STATE_EXIT;
+                        current_state = STATE_ACHIEVE;
                         break;
+                    case 3:
+                        current_state = STATE_EXIT;
+                        break;    
                 }
 
                 break;
@@ -162,14 +174,14 @@ int main() {
             // Info
             case STATE_HTP:
                 // How to play
-                cout << default_format << 3 << ";" << startCol - (how_to_play[0].length())/2 << "H" << how_to_play[0] << endl; // title
-                cout << default_format << 10 << ";" << startCol - (how_to_play[1].length())/2 << "H" << how_to_play[1] << endl; // keys
-                cout << default_format << 12 << ";" << startCol - (how_to_play[2].length())/2 << "H" << how_to_play[2] << endl;
-                cout << default_format << 14 << ";" << startCol - (how_to_play[3].length())/2 << "H" << how_to_play[3] << endl;
-                cout << default_format << 16 << ";" << startCol - (how_to_play[4].length())/2 << "H" << how_to_play[4] << endl;
-                cout << default_format << 20 << ";" << startCol - (how_to_play[5].length())/2 << "H" << how_to_play[5] << endl; // words
-                cout << default_format << 24 << ";" << startCol - (how_to_play[6].length())/2 << "H" << how_to_play[6] << endl;
-                cout << default_format << 28 << ";" << startCol - (how_to_play[7].length())/2 << "H" << how_to_play[7] << endl;
+                cout << default_format << 10 << ";" << startCol - (how_to_play[0].length())/2 << "H" << how_to_play[0] << endl; // title
+                cout << default_format << 17 << ";" << startCol - (how_to_play[1].length())/2 << "H" << how_to_play[1] << endl; // keys
+                cout << default_format << 19 << ";" << startCol - (how_to_play[2].length())/2 << "H" << how_to_play[2] << endl;
+                cout << default_format << 21 << ";" << startCol - (how_to_play[3].length())/2 << "H" << how_to_play[3] << endl;
+                cout << default_format << 23 << ";" << startCol - (how_to_play[4].length())/2 << "H" << how_to_play[4] << endl;
+                cout << default_format << 25 << ";" << startCol - (how_to_play[5].length())/2 << "H" << how_to_play[5] << endl; // words
+                cout << default_format << 27 << ";" << startCol - (how_to_play[6].length())/2 << "H" << how_to_play[6] << endl;
+                cout << default_format << 30 << ";" << startCol - (how_to_play[7].length())/2 << "H" << how_to_play[7] << endl;
 
                 keyboard.get_userInput();
 
@@ -179,15 +191,26 @@ int main() {
                 
                 break;
 
-           // Game
-            case STATE_GAME:
-                cout << "Game started" << endl;
+            case STATE_ACHIEVE:
+                cout << "Achievements" << endl;
+                // to be implemented
+                keyboard.get_userInput();
 
                 // Exit to menu
-                if (keyboard.key == 'q')
+                if (keyboard.key == KEY_EXIT)
                     current_state = STATE_MENU;
+                break;
+           // Game
+            case STATE_GAME:
+                // Exit to menu
+                if (keyboard.key != KEY_EXIT)
+                    Hero_run();
+                    
+                current_state = STATE_MENU;    
                 break; 
 
+                
+                
             case STATE_EXIT:
                 cout << "Exiting game" << endl;
                 break;
@@ -197,6 +220,7 @@ int main() {
     
     // Exit
     cout << restore_cursor_position; // Restore cursor position
+    cout << clear_screen; // Clear screen
     cout << show_cursor; // Show cursor
     return 0;
     
