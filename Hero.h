943b67d29ff88ba8dbaa3d348c854fc1f819135c
KY <162005150+Atar369@users.noreply.github.com>
2024-04-23 20:15:59 +0800

    Create Hero.h

---
+++ Hero.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include "keyboard.h"
+#include "draw.h"
+#include "maps.h"
+#include "window.h"
+using namespace std;
+
+void Hero_run();
