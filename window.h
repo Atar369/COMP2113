64641201148997e634ff68e670627b6de6d94a18
KY <162005150+Atar369@users.noreply.github.com>
2024-04-24 14:27:28 +0800

    Update window.h

--- window.h
+++ window.h
@@ -9,23 +9,33 @@
 #include "keyboard.h"
 #include "draw.h"
 #include "maps.h"
+#include "player.h"
 using namespace std;
 
 
 class Window {
+    
     int choice_button = 0;
-    bool choice_made = false;
     int x_coor = 0, y_coor = MAP_HEIGHT * 2 / 3;
     int width = MAP_WIDTH*3;
     int height = MAP_HEIGHT/2;
     vector<vector<string> > window_buffer;
 
+    // Save options
+    int select_yes, select_no;
+    vector<string> save_options = {"> YES <", "  Yes  ", "> NO <", "  No  "};
+    vector<vector<short> > temp_map;
+
     public:
-        int scn_num = 0;
-        int event_num = 1;
-        void build_buffer(const string & content) ;
+        bool is_Hero = false;
+        bool is_Girl = false;
+        void build_buffer(const string & content);
         void reset_buffer();
-        void Print_buffer(vector<vector<short> > current_map);
-        void handle_choice(vector<vector<short> >current_map);
+        void Print_buffer(vector<vector<short> > current_map, Player &player, string color);
+        void intro_character_choice(Player &player);
+        void handle_choice(vector<vector<short> > current_map, int &scn_num, int &event_num, Player &player);
+        void handle_save_choice(int &choice_button, int &map_code, Player &player);
 
 };
+
+extern Window window;
