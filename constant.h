2d80cac07ed48d48ed3ef7e03c27bbe8afda954a
KristyLWY <162736551+KristyLWY@users.noreply.github.com>
2024-04-24 22:50:20 +0800

    Update constant.h

--- constant.h
+++ constant.h
@@ -23,199 +23,212 @@ using namespace std;
 //int w, h;
 
 // Color
-const short c_empty         = 0;
-const short c_dtopleft     = 1;
-const short c_dtopright    = 2;
-const short c_dbottomleft  = 3;
-const short c_dbottomright = 4;
-const short c_dhorizontal  = 5;
-const short c_dverticalleft = 6;
-const short c_dverticalright = 7;
-const short c_dverticalmiddle = 8;
-const short c_dtopcross = 9;
-const short c_dbottomcross = 10;
-const short c_dleftcross = 11;
-const short c_drightcross = 12;
-const short c_dmiddlecross = 13;
-const short c_stopleft = 14;
-const short c_srightleft = 15;
-const short c_sbottomleft = 16;
-const short c_sbottomright = 17;
-const short c_shorizontal = 18;
-const short c_sverticalleft = 19;
-const short c_sverticalright = 20;
-const short c_sverticalmiddle = 21;
-const short c_stopcross = 22;
-const short c_sbottomcross = 23;
-const short c_sleftcross = 24;
-const short c_srightcross = 25;
-const short c_smiddlecross = 26;
-const short c_bed = 27;
-const short c_bedleft = 28;
-const short c_bedright = 29;
-const short c_bedcentre = 30;
-const short c_bedmiddle = 31;
-const short c_leftchair = 32;
-const short c_rightchair = 33;
-const short c_door = 34;
-const short c_leftdoor = 35;
-const short c_rightdoor = 36;
-const short c_lightoff = 37;
-const short c_lighton = 38;
-const short c_treasure = 39;
-const short c_key = 40;
-const short c_leftcupboard = 41;
-const short c_rightcupboard = 42;
-const short c_npc = 43;
-const short c_tree = 44;
-const short c_treebranch = 45;
-const short c_treeleft = 46;
-const short c_treeright = 47;
-const short c_grass = 48;
-const short c_leaveleft = 49;
-const short c_leaveright = 50;
-const short c_dmbottomleft = 51;
-const short c_dmbottomright = 52;
-const short c_bush = 53;
-const short c_brick = 54;
-const short c_castle = 55;
-const short c_flag = 56;
-const short c_hole = 57;
-const short c_stairs1 = 58;
-const short c_stairs2 = 59;
-const short c_stairs3 = 60;
-const short c_stairs4 = 61;
-const short c_stairs5 = 62;
-const short c_stairs6 = 63;
-const short c_stairs7 = 64;
-const short c_dragonnpc = 65;
-const short c_dragon = 66;
-const short c_deaddragonnpc = 67;
-const short c_deaddragon = 68;
-const short c_player = 69;
-const short c_oldman = 70;
-const short c_rooftop1 = 71;
-const short c_rooftop2 = 72;
-const short c_rooftop3 = 73;
-const short c_rooftop4 = 74;
-const short c_knob = 75;
-const short c_rooftop5 = 76;
-const short c_rooftop6 = 77;
+const short c_empty = 0;
+const short c_wall = 1;
+const short c_hero = 2;
+const short c_girl = 3;
+const short c_bed = 4;
+const short c_bedleft = 5;
+const short c_bedright = 6;
+const short c_bedcentre = 7;
+const short c_bedmiddle = 8;
+const short c_leaveleft = 9;
+const short c_leaveright = 10;
+const short c_table = 11;
+const short c_chair = 12;
+const short c_door = 13;
+const short c_leftdoor = 14;
+const short c_rightdoor = 15;
+const short c_lightoff = 16;
+const short c_lighton = 17;
+const short c_treasure = 18;
+const short c_key = 19;
+const short c_leftcupboard = 20;
+const short c_rightcupboard = 21;
+const short c_npc = 22;
+const short c_tree = 23;
+const short c_treebranch = 24;
+const short c_treeleft = 25;
+const short c_treeright = 26;
+const short c_grass = 27;
+const short c_bush = 28;
+const short c_brick = 29;
+const short c_castle = 30;
+const short c_flag = 31;
+const short c_hole = 32;
+const short c_stairs1 = 33;
+const short c_stairs2 = 34;
+const short c_stairs3 = 35;
+const short c_stairs4 = 36;
+const short c_stairs5 = 37;
+const short c_stairs6 = 38;
+const short c_stairs7 = 39;
+const short c_dragonnpc = 40;
+const short c_dragon = 41;
+const short c_deaddragonnpc = 42;
+const short c_deaddragon = 43;
+const short c_oldman = 44;
+const short c_rooftop1 = 45;
+const short c_rooftop2 = 46;
+const short c_rooftop3 = 47;
+const short c_rooftop4 = 48;
+const short c_rooftop5 = 49;
+const short c_rooftop6 = 50;
+const short c_knob = 51;
+const short c_leftwardrobe = 52;
+const short c_rightwardrobe = 53;
+const short c_librarian = 54;
+const short c_penny = 55;
+const short c_haley = 56;
+const short c_cooper = 57;
+const short c_robert = 58;
+const short c_castlewall = 59;
+const short c_stairs8 = 60;
+const short c_stairs9 = 61;
+const short c_chair1 = 62;
+const short c_chair2 = 63;
+const short c_chair3 = 64;
+const short c_chair4 = 65;
+const short c_chair5 = 66;
+const short c_chair6 = 67;
+const short c_chair7 = 68;
+const short c_chair8 = 69;
+const short c_chair9 = 70;
+const short c_chair10 = 71;
+const short c_chair11 = 72;
+const short c_chair12 = 73;
+const short c_chair13 = 74;
+const short c_chair14 = 75;
+const short c_chair15 = 76;
+const short c_chair16 = 77;
+const short c_chair17 = 78;
+const short c_chair18 = 79;
+const short c_chair19 = 80;
+const short c_lamp1 = 81;
+const short c_lamp2 = 82;
+const short c_lamp3 = 83;
+const short c_lamp4 = 84;
+const short c_lamp5 = 85;
+const short c_lamp6 = 86;
+const short c_lamp7 = 87;
+const short c_lamp8 = 88;
+const short c_chair20 = 89;
+const short c_chair21 = 90;
+const short c_chair22 = 91;
+const short c_chair23 = 92;
+const short c_lamp9 = 93;
+const short c_lamp10 = 94;
 
 #define i_empty        0
-#define i_c_dtopleft   1
-#define i_c_dtopright  2
-#define i_c_dbottomleft 3
-#define i_c_dbottomright 4
-#define i_c_dhorizontal 5
-#define i_c_dverticalleft 6
-#define i_c_dverticalright 7
-#define i_c_dverticalmiddle 8
-#define i_c_dtopcross  9
-#define i_c_dbottomcross 10
-#define i_c_dleftcross 11
-#define i_c_drightcross 12
-#define i_c_dmiddlecross 13
-#define i_c_stopleft   14
-#define i_c_srightleft 15
-#define i_c_sbottomleft 16
-#define i_c_sbottomright 17
-#define i_c_shorizontal 18
-#define i_c_sverticalleft 19
-#define i_c_sverticalright 20
-#define i_c_sverticalmiddle 21
-#define i_c_stopcross 22
-#define i_c_sbottomcross 23
-#define i_c_sleftcross 24
-#define i_c_srightcross 25
-#define i_c_smiddlecross 26
-#define i_c_bed 27
-#define i_c_bedleft 28
-#define i_c_bedright 29
-#define i_c_bedcentre 30
-#define i_c_bedmiddle 31
-#define i_c_leftchair 32
-#define i_c_rightchair 33
-#define i_c_door 34
-#define i_c_leftdoor 35
-#define i_c_rightdoor 36
-#define i_c_lightoff 37
-#define i_c_lighton 38
-#define i_c_treasure 39
-#define i_c_key 40
-#define i_c_leftcupboard 41
-#define i_c_rightcupboard 42
-#define i_c_npc 43
-#define i_c_tree 44
-#define i_c_treebranch 45
-#define i_c_treeleft 46
-#define i_c_treeright 47
-#define i_c_grass 48
-#define i_c_leaveleft 49
-#define i_c_leaveright 50
-#define i_c_dmbottomleft 51
-#define i_c_dmbottomright 52
-#define i_c_bush 53
-#define i_c_brick 54
-#define i_c_castle 55
-#define i_c_flag 56
-#define i_c_hole 57
-#define i_c_stairs1 58
-#define i_c_stairs2 59
-#define i_c_stairs3 60
-#define i_c_stairs4 61
-#define i_c_stairs5 62
-#define i_c_stairs6 63
-#define i_c_stairs7 64
-#define i_c_dragonnpc 65
-#define i_c_dragon 66
-#define i_c_deaddragonnpc 67
-#define i_c_deaddragon 68
-#define i_c_player 69
-#define i_c_oldman 70
-#define i_c_rooftop1 71
-#define i_c_rooftop2 72
-#define i_c_rooftop3 73
-#define i_c_rooftop4 74
-#define i_c_knob 75
-#define i_c_rooftop5 76
-#define i_c_rooftop6 77
+#define i_wall         1
+#define i_hero         2
+#define i_girl         3
+#define i_bed          4
+#define i_bedleft      5
+#define i_bedright     6
+#define i_bedcentre    7
+#define i_bedmiddle    8
+#define i_leaveleft    9
+#define i_leaveright   10
+#define i_table        11
+#define i_chair        12
+#define i_door         13
+#define i_leftdoor     14
+#define i_rightdoor    15
+#define i_lightoff     16
+#define i_lighton      17
+#define i_treasure     18
+#define i_key          19
+#define i_leftcupboard 20
+#define i_rightcupboard 21
+#define i_npc          22
+#define i_tree         23
+#define i_treebranch   24
+#define i_treeleft     25
+#define i_treeright    26
+#define i_grass        27
+#define i_bush         28
+#define i_brick        29
+#define i_castle       30
+#define i_flag         31
+#define i_hole         32
+#define i_stairs1      33
+#define i_stairs2      34
+#define i_stairs3      35
+#define i_stairs4      36
+#define i_stairs5      37
+#define i_stairs6      38
+#define i_stairs7      39
+#define i_dragonnpc    40
+#define i_dragon       41
+#define i_deaddragonnpc 42
+#define i_deaddragon   43
+#define i_oldman       44
+#define i_rooftop1     45
+#define i_rooftop2     46
+#define i_rooftop3     47
+#define i_rooftop4     48
+#define i_rooftop5     49
+#define i_rooftop6     50
+#define i_knob         51
+#define i_leftwardrobe 52
+#define i_rightwardrobe 53
+#define i_librarian    54
+#define i_penny        55
+#define i_haley        56
+#define i_cooper       57
+#define i_robert       58
+#define i_castlewall   59
+#define i_stairs8      60
+#define i_stairs9      61
+#define i_chair1 62
+#define i_chair2 63
+#define i_chair3 64
+#define i_chair4 65
+#define i_chair5 66
+#define i_chair6 67
+#define i_chair7 68
+#define i_chair8 69
+#define i_chair9 70
+#define i_chair10 71
+#define i_chair11 72
+#define i_chair12 73
+#define i_chair13 74
+#define i_chair14 75
+#define i_chair15 76
+#define i_chair16 77
+#define i_chair17 78
+#define i_chair18 79
+#define i_chair19 80
+#define i_lamp1 81
+#define i_lamp2 82
+#define i_lamp3 83
+#define i_lamp4 84
+#define i_lamp5 85
+#define i_lamp6 86
+#define i_lamp7 87
+#define i_lamp8 88
+#define i_chair20 89
+#define i_chair21 90
+#define i_chair22 91
+#define i_chair23 92
+#define i_lamp9 93
+#define i_lamp10 94
 
 // Symbol map object
-#define s_empty         "   "
-#define s_dtopleft     "╔══"
-#define s_dtopright    "══╗"
-#define s_dbottomleft  "╚══"
-#define s_dbottomright "══╝"
-#define s_dhorizontal  "═══"
-#define s_dverticalleft "║  "
-#define s_dverticalright "  ║"
-#define s_dverticalmiddle " ║ "
-#define s_dtopcross "═╦═"
-#define s_dbottomcross "═╩═"
-#define s_dleftcross "╠══"
-#define s_drightcross "══╣"
-#define s_dmiddlecross "═╬═"
-#define s_stopleft "┌──"
-#define s_srightleft "──┐"
-#define s_sbottomleft "└──"
-#define s_sbottomright "──┘"
-#define s_shorizontal "───"
-#define s_sverticalleft "│  "
-#define s_sverticalright "  │"
-#define s_sverticalmiddle " │ "
-#define s_stopcross "─┬─"
-#define s_sbottomcross "─┴─"
-#define s_sleftcross "├──"
-#define s_srightcross "──┤"
-#define s_smiddlecross "─┼─"
+#define s_empty        "   "
+#define s_wall "///"
+#define s_hero "/@\\"
+#define s_girl "/%\\"
 #define s_bed "==="
 #define s_bedleft "== "
 #define s_bedright " =="
 #define s_bedcentre " = "
 #define s_bedmiddle "= ="
-#define s_leftchair "║██"
-#define s_rightchair "██║"
+#define s_leaveleft "=  "
+#define s_leaveright "  ="
+#define s_table "[T]"
+#define s_chair "|-|"
 #define s_door "---"
 #define s_leftdoor "{--"
 #define s_rightdoor "--}"
@@ -223,22 +236,18 @@ const short c_rooftop6 = 77;
 #define s_lighton "[`]"
 #define s_treasure "{$}"
 #define s_key "[*]"
-#define s_leftcupboard "│ ═"
-#define s_rightcupboard "═ │"
+#define s_leftcupboard "| ="
+#define s_rightcupboard "= |"
 #define s_npc "/N\\"
 #define s_tree "|=|"
 #define s_treebranch "/=\\"
 #define s_treeleft "  /"
 #define s_treeright "\\  "
 #define s_grass "wWw"
-#define s_leaveleft "=  "
-#define s_leaveright "  ="
-#define s_dmbottomleft " ╚═"
-#define s_dmbottomright "═╝ "
 #define s_bush " _ "
-#define s_brick "║║║"
-#define s_castle "_═_"
-#define s_flag " ⚑ "
+#define s_brick "|||"
+#define s_castle "_=_"
+#define s_flag " |-"
 #define s_hole " o "
 #define s_stairs1 "  |"
 #define s_stairs2 "|  "
@@ -249,14 +258,56 @@ const short c_rooftop6 = 77;
 #define s_stairs7 "___"
 #define s_dragonnpc "^N^"
 #define s_dragon "^@^"
-#define s_deaddragonnpc "^░^"
-#define s_deaddragon "^▒^"
-#define s_player "/@\\"
-#define s_oldman "/▼\\"
+#define s_deaddragonnpc "xNx"
+#define s_deaddragon "x@x"
+#define s_oldman "/V\\"
 #define s_rooftop1 " /_"
 #define s_rooftop2 "/  "
 #define s_rooftop3 "_\\ "
 #define s_rooftop4 "  \\"
-#define s_knob "  ."
 #define s_rooftop5 " __"
 #define s_rooftop6 "__ "
+#define s_knob "  ."
+#define s_leftwardrobe "|=="
+#define s_rightwardrobe "==|"
+#define s_librarian "/L\\"
+#define s_penny "/P\\"
+#define s_haley "/H\\"
+#define s_cooper "/C\\"
+#define s_robert "/R\\"
+#define s_castlewall " | "
+#define s_stairs8 "/__"
+#define s_stairs9 "__\\"
+#define s_chair1 "| |"
+#define s_chair2 "/_/"
+#define s_chair3 "\\_\\"
+#define s_chair4 "|__"
+#define s_chair5 "__|"
+#define s_chair6 " .."
+#define s_chair7 ".. "
+#define s_chair8 " ' "
+#define s_chair9 "/ /"
+#define s_chair10 "_//"
+#define s_chair11 "\\\\_"
+#define s_chair12 "|:."
+#define s_chair13 ".:|"
+#define s_chair14 "o) "
+#define s_chair15 " (o"
+#define s_chair16 " |_"
+#define s_chair17 "_| "
+#define s_chair18 "|__"
+#define s_chair19 "__|"
+#define s_lamp1 "  _"
+#define s_lamp2 "_  "
+#define s_lamp3 "/'-"
+#define s_lamp4 "-'\\"
+#define s_lamp5 "  ."
+#define s_lamp6 ".  "
+#define s_lamp7 " '|"
+#define s_lamp8 "|' "
+#define s_chair20 "..."
+#define s_chair21 "  '"
+#define s_chair22 "|_|"
+#define s_chair23 "\\ \\"
+#define s_lamp9 " _|"
+#define s_lamp10 "|_ "
