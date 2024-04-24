35b75ce52b11205a711c20b7759f42850c2b8806
KY <162005150+Atar369@users.noreply.github.com>
2024-04-25 00:11:42 +0800

    Update Hero.h

--- Hero.h
+++ Hero.h
@@ -4,12 +4,13 @@
 #include <string>
 #include <iomanip>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
 #include "keyboard.h"
 #include "draw.h"
 #include "maps.h"
 #include "window.h"
+#include "chat.h"
 using namespace std;
 
-void Hero_run();
+
+void Hero_run(int &scn_num, int &map_code, int &event_num, Player &player);
