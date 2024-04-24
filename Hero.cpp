2eb66a243964a8b07e7e1670dfebbeaae4c18ab6
KY <162005150+Atar369@users.noreply.github.com>
2024-04-24 14:42:32 +0800

    Update Hero.cpp

--- Hero.cpp
+++ Hero.cpp
@@ -1,34 +1,66 @@
 #include "Hero.h"
 
 // testing
-void Hero_run() {
-
+void Hero_run(int &scn_num, int &map_code, int &event_num, Player &player) {
     typedef enum {
     hero_house,
     girl_house,
-    villager_house,
-    village_top_left,
-    village_top_right,
-    village_bottom_left,
-    village_bottom_right,
-    woods,
-    puzzle,
+    oldman_house,
+    village,
+    outside_village,
+    forest,
+    monster,
     castle,
+    back_village,
+    all_dead,
     } map_transition;
 
-    // Player - Hero
-    player.x = 4;
-    player.y = 4;
-    player.symbol = "|@|";
-
-    Window window;
-
     vector<vector<short> > current_map;
-    
-    map_transition map_state;
+        
+    map_transition map_state;    
 
-    int map_code = 0;
-    map_state = hero_house; // Hero start at hero house
+    switch(map_code) {
+        case 0:
+            map_state = hero_house;
+        break;
+
+        case 1:
+            map_state = girl_house;
+        break;
+
+        case 2:
+            map_state = oldman_house;
+        break;
+
+        case 3:
+            map_state = village;
+        break;
+
+        case 4:
+            map_state = outside_village;
+        break;
+
+        case 5:
+            map_state = forest;
+        break;
+
+        case 6:
+            map_state = monster;
+        break;
+
+        case 7:
+            map_state = back_village;
+        break;
+
+        case 8:
+            map_state = all_dead;
+        break;
+
+        case 9:
+            map_state = castle;
+        break;
+    }
+    
     current_map = map_code_mapping.at(map_code);
 
     while (true) {
@@ -41,11 +73,12 @@ void Hero_run() {
 
             case hero_house: // hero house
                 map_code = 0;
-                if (window.event_num == 1) {
-                    window.handle_choice(current_map);
-                }
-                if (current_map[player.y][player.x] == i_door)  {
-                    map_state = village_top_left;
+
+                if (event_num == 1)
+                    window.handle_choice(current_map, scn_num, event_num, player);
+                
+                if (current_map[player.y][player.x] == i_c_door)  {
+                    map_state = village;
                     offsety = 6;
                 }
                 // door transition    
@@ -53,36 +86,60 @@ void Hero_run() {
             
             case girl_house: // girl's house
                 map_code = 1;
-                if (current_map[player.y][player.x] == i_door)  {
-                    map_state = village_top_right;
+                if (current_map[player.y][player.x] == i_c_door)  {
+                    map_state = village;
                     offsety = 10;
                 }
             break;
 
-            case villager_house: 
+            case oldman_house: 
                 map_code = 2;
-                if (current_map[player.y][player.x] == i_door)  {
-                    map_state = village_top_right;
+                if (current_map[player.y][player.x] == i_c_door)  {
+                    map_state = village;
                     //player.y = 15;
                 }
             break;
 
-            case village_top_left:
+            case village:
                 map_code = 3;
                 if (player.x >= MAP_HEIGHT) {
-                    map_state = village_bottom_left;
+                    map_state = forest;
                     //player.x = 1;
                 }
                 if (player.y >= MAP_WIDTH) {
                     //player.y = 1;
-                    map_state = village_top_right;
+                    map_state = monster;
                 }
             break;
+
+            case outside_village:
+                map_code = 4;
+            break;
+
+            case forest:
+                map_code = 5;
+            break;
+
+            case monster:
+                map_code = 6;
+            break;
+            
+            case castle:
+                map_code = 7;
+            break;
+
+            case back_village:
+                map_code = 8;
+            break;
+
+            case all_dead:
+                map_code = 9;
+            break; 
         }
         
         current_map = map_code_mapping.at(map_code); 
 
-        draw_map(current_map);
+        draw_map(current_map, player);
 
         player.x -= offsetx;
         player.y -= offsety;
@@ -97,21 +154,21 @@ void Hero_run() {
             break;
         }
         if (keyboard.key == KEY_UP) {
-            moveUp();
+            player.moveUp();
         }
         if (keyboard.key == KEY_DOWN) {
-            moveDown();
+            player.moveDown();
         }
         if (keyboard.key == KEY_LEFT) {
-            moveLeft();
+            player.moveLeft();
         }
         if (keyboard.key == KEY_RIGHT) {
-            moveRight();
+            player.moveRight();
         }
 
         // Move the player
-        player_move(keyboard.key, current_map);
-        player_collision(current_map);
+        player.player_move(keyboard.key, current_map);
+        player.player_collision(current_map);
     }
 }
 
