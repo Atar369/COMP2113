f56f76b580e39a718227e1a819346adbf09e804f
KY <162005150+Atar369@users.noreply.github.com>
2024-04-25 00:11:18 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Menu
-const char* menu_logo[7] = {
+const vector<string> menu_logo = {
 "░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░      ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░░▒▓██████▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓████████▓▒░", 
 "░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░                ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░",        
 "░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░                ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░",        
@@ -13,20 +13,7 @@ const char* menu_logo[7] = {
 "░▒▓████████▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓████████▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░" 
 };
 
-
-// Get logo width
-int get_logo_w_size() {
-    int logo_w_size = 0;
-
-    for (int i = 0; i < logo_h_size; i++) {
-        int len = strlen(menu_logo[i]);
-        if (len > logo_w_size) {
-            logo_w_size = len;
-        }
-    }
-    return logo_w_size;
-}
-
+/*
 // Draw the logo
 void draw_logo(int h, int w) {
     // Clear
@@ -37,7 +24,7 @@ void draw_logo(int h, int w) {
         cout << font_blue << default_format << logo_h_size + i << ";" << 37 << "H" << line << endl;
     }
     cout << reset;
-}
+}*/
 
 
 /*
