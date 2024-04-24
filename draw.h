5c6be729a3f0ff8318ef1e377ae1024ddfd461e1
KY <162005150+Atar369@users.noreply.github.com>
2024-04-25 00:12:50 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include <math.h>
 #include <string>
-#include <map>
-//#include "maps.h"
-//#include "object.h"
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+//#include <unistd.h>
+//#include <sys/ioctl.h>
 #include "format.h"
 #include "player.h"
+
 
 using namespace std;
 
 void draw_instance(int y, int x, int obj, string symbol);
 
-//bool next_map(short current_map[ROWS][COLS], short &map_code);
+//bool next_map(vector<vector<string> > current_map, short &map_code);
 
-void draw_map(short current_map[ROWS][COLS]);
+void draw_map(vector<vector<short> > &current_map, Player &player);
