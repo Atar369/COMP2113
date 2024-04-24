882f843db7eb4ad67b98887c33a1e0dc422089ca
KY <162005150+Atar369@users.noreply.github.com>
2024-04-24 14:32:46 +0800

    Update player.h

--- player.h
+++ player.h
@@ -6,29 +6,52 @@
 #include <cstring>
 #include "keyboard.h"
 #include "constant.h"
+#include "object.h"
 
 
 using namespace std;
 
-struct object {
+class Player {
+    public:
     int hrz, vtrl;
     int x, y;
     int direction;
     string symbol;
+    string color;
+
+    public:
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+
+    void stopMovement();
+
+    void player_move(int key, vector<vector<short> > &current_map);
+
+    void player_collision(vector<vector<short> > &current_map);
 };
 
-extern object player;
+/*
+struct objects {
+    int key = 0;
+    //int sword;
+    int treasure = 0;
+    int light_switch;
+    //int closet;
+    //int wall;
+    int npc = 0;
+    int monster = 0;
+}; */
 
-void moveUp();
 
-void moveDown();
 
-void moveLeft();
 
-void moveRight();
+//void updatePlayercorr(short current_map[ROWS][COLS]);
 
-void stopMovement();
+//void displayMap(short current_map[ROWS][COLS]);
 
-void player_move(int key, short current_map[ROWS][COLS]);
 
-void player_collision(short current_lvl[][COLS]);
