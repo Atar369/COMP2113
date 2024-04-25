0c8a98d79c44c6e63f7850a96056e4f2d84b1f74
KristyLWY <162736551+KristyLWY@users.noreply.github.com>
2024-04-26 01:31:49 +0800

    Create maintest.h

---
+++ maintest.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <sys/ioctl.h>
+#include <string>
+#include <unistd.h>
+#include <fstream>
+#include "menu.h"
+#include "Hero.h"
+#include "progress.h"
+using namespace std;
