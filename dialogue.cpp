6e18eaa8f92d511d54b010cb50570a3010130f86
Kwokyw <160570241+Kwokyw@users.noreply.github.com>
2024-04-27 06:09:09 +0800

    Update dialogue.cpp

--- dialogue.cpp
+++ dialogue.cpp
@@ -32,8 +32,35 @@ int main(){
   "You: TIME REWIND!!!"
   "(The apple on your hand became a fresh, fragant apple in a flash)"
   "You: Oh my god... this is real!"
-  "(You were so panic and you wanted to tell some other people about this.)"
-  "(You ran out of your house.)"
+  "(You were so panic but you were trying to be claim)"
+  "You: If I tell the others that I can rewind time, mayebe I will be kidnapped and exploited."
+  "(You were pondering.)"
+  "You: Time rewind... the effect of this power is actually similar to healing."
+  "You: Maybe I can pretend to be a healer using this ability."
+  "You: If people get hurted, I can fix their body by rewinding the time to the moment before ther are hurted."
+  "You: It is easy for me to pretend to be a healer."
+  "(You decided to hide your real abillity and pretend to be a healer.)"
+  "(After you decided to hide your true power, you were going to the forest.)"
+  "(You got lost in the forest.)"
+  "(You saw a monster in the distance.)"
+  "(It was a huge monster, with sharp teeth and red eyes.)"
+  "(However, you saw many bleeding knife wounds on its body.)"
+  "(You decided to...)"
+  "(Run away          Rescue it)"
+  
+  //scene2 (branch 1)
+  "(You decided to run away.)"
+  "(However, the monster jumped to you)"
+  "(The monster chased up you.)"
+  "Monster: ARRRWWWWWWW, human! Die!"
+  "(The monster ate you.)"
+  "(You are dead)"
+  "Ending: Cuisine"
+  
+  //scene3 (branch 2)
+  "(You decided to rescue it.)"
+  "Monster: You... saved me?"
+  "You: Yes, as you seemed not feeling well."
   ""
 }
 
