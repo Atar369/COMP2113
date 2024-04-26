3024ed88d9d6b9db058a3bab6fc40c3dc675f444
KY <162005150+Atar369@users.noreply.github.com>
2024-04-27 00:13:28 +0800

    Update progress.cpp

--- progress.cpp
+++ progress.cpp
@@ -8,6 +8,16 @@ void Progress::save_progress(Player player) {
     fout << this->scn_num << endl;
     fout << this->map_code << endl;
     fout << this->event_num << endl;
+    fout << this->can_enterMonster << endl;
+    fout << this->can_enterCastle << endl;
+    fout << this->first_time_entering_castle << endl;
+    fout << this->first_time_entering_store << endl;
+    fout << this->talked_to_oldman << endl;
+    fout << this->talked_to_robert << endl;
+    fout << this->saw_monster << endl;
+    fout << this->monster_killed << endl;
+    fout << this->dragonnpc_killed << endl;
+    fout << this->dragon_killed << endl;
     fout << player.x << endl;
     fout << player.y << endl;
     fout << player.color << endl;
@@ -36,6 +46,16 @@ void Progress::load_progress(Player &player) {
     fin >> this->scn_num;
     fin >> this->map_code;
     fin >> this->event_num;
+    fin >> this->can_enterMonster;
+    fin >> this->can_enterCastle;
+    fin >> this->first_time_entering_castle;
+    fin >> this->first_time_entering_store;
+    fin >> this->talked_to_oldman;
+    fin >> this->talked_to_robert;
+    fin >> this->saw_monster;
+    fin >> this->monster_killed;
+    fin >> this->dragonnpc_killed;
+    fin >> this->dragon_killed;
     fin >> player.x;
     fin >> player.y;
     fin >> player.color;
@@ -51,6 +71,16 @@ void Progress::reset_progress() {
     this->map_code = 0;
     this->event_num = 0;
     this->ending_num = 0;
+    this->can_enterMonster = 1;
+    this->can_enterCastle = 1;
+    this->first_time_entering_castle = 1;
+    this->first_time_entering_store = 1;
+    this->talked_to_oldman = 0;
+    this->talked_to_robert = 0;
+    this->saw_monster = 0;
+    this->monster_killed = 0;
+    this->dragonnpc_killed = 0;
+    this->dragon_killed = 0;
 }
 
 void Progress::delete_progress() {
