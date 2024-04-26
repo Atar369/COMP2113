6faaf5964ef8c9f8e5b52a4a77bcfb091d2e8a82
KY <162005150+Atar369@users.noreply.github.com>
2024-04-27 00:15:13 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -8,6 +8,7 @@
 #include <sys/ioctl.h>
 #include "format.h"
 #include "player.h"
+#include "maps.h"
 
 
 using namespace std;
@@ -17,3 +18,7 @@ void draw_instance(int y, int x, int obj, string symbol);
 //bool next_map(vector<vector<string> > current_map, short &map_code);
 
 void draw_map(vector<vector<short> > &current_map, Player &player);
+
+void change_map(vector<vector<short> > & current_map, int target, int new_obj);
+
+//void change_after_touch(vector<vector<short> > &current_map, Player &player);
