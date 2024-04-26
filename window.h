52891952b4bcf39964536ce91e16b3bfb48ba2c3
KY <162005150+Atar369@users.noreply.github.com>
2024-04-26 13:58:06 +0800

    Update window.h

--- window.h
+++ window.h
@@ -2,16 +2,15 @@
 
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
-#include <unistd.h>
-#include <sys/ioctl.h>
+//#include <unistd.h>
+//#include <sys/ioctl.h>
 #include "keyboard.h"
 #include "draw.h"
 #include "maps.h"
 #include "player.h"
+
 using namespace std;
 
 
@@ -26,17 +25,18 @@ class Window {
     // Save options
     int select_yes, select_no;
     vector<string> save_options = {"> YES <", "  Yes  ", "> NO <", "  No  "};
-    vector<vector<short> > temp_map;
 
     public:
         bool is_Hero = false;
         bool is_Girl = false;
+
         void build_buffer(const string & content);
         void reset_buffer();
         void Print_buffer(vector<vector<short> > current_map, Player &player, string color);
         void intro_character_choice(Player &player);
-        void handle_choice(vector<vector<short> > current_map, int &scn_num, int &event_num, Player &player);
+        void handle_choice(vector<vector<short> > current_map, int &scn_num, int &event_num, int &ending_num, Player &player);
         void handle_save_choice(int &choice_button, int &map_code, Player &player);
+        void Print_endings(vector<string> contents, string color);
 
 };
 
