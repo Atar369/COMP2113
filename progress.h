16fac7e592698d5e680272216a60eee7efd8cacc
KY <162005150+Atar369@users.noreply.github.com>
2024-04-27 22:34:14 +0800

    Update progress.h

--- progress.h
+++ progress.h
@@ -13,16 +13,32 @@ class Progress {
     int map_code;
     int event_num;
     int ending_num;
+
+    // for hero
     bool can_enterMonster;
     bool can_enterCastle;
     bool first_time_entering_castle;
     bool first_time_entering_store;
     bool talked_to_oldman;
     bool talked_to_robert;
+    bool talked_to_cooper;
+    bool saw_key;
+    bool saw_treasure;
     bool saw_monster;
+    bool monster_noticed;
     bool monster_killed;
     bool dragonnpc_killed;
     bool dragon_killed;
+    bool get_treasure1;
+    bool get_treasure2;
+    bool get_treasure3;
+    bool all_treasure;
+    int key_take_count;
+    bool hero_killed_all;
+
+    // for girl
+    bool girl_saved_monster;
+    bool girl_rewind;
 
     public:
         void save_progress(Player player);
