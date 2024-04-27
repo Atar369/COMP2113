eed0dd995b0d268a7b3bf48306cff93be7ce1f10
KristyLWY <162736551+KristyLWY@users.noreply.github.com>
2024-04-28 00:06:10 +0800

    Create Girl.h

---
+++ Girl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <unordered_map>
+#include "keyboard.h"
+#include "draw.h"
+#include "maps.h"
+#include "window.h"
+#include "chat.h"
+#include "progress.h"
+using namespace std;
+
+
+void Girl_run(Progress &progress, Player &player);
