214e8037c8c5ccf0a52905bf35d8cd9cf7e52f85
KristyLWY <162736551+KristyLWY@users.noreply.github.com>
2024-04-29 00:02:47 +0800

    Update Girl.cpp

--- Girl.cpp
+++ Girl.cpp
@@ -3,38 +3,40 @@
 unordered_map<int, vector<string> > Girl_endings = {
 
     {1, { //killed by monster (not rescuing him)
-        "You saw monsters everywhere.",
-        "You was scared.",
-        "You ran away.",
-        "Ran as far as you could.",
-        "Until you went back to the village.",
-        "Rushed into your house.",
-        "You was safe.",
-        "..."
-        "You chosed to be a Hero.",
-        "But you scare to be a Hero.",
-        "One day, you get out of your room. You saw another 'Hero'.",
-        "'He' was the one, you know it.",
-        "With 'his' powerful sword, shining under the sunlight.",
-        "His eyes, full of courage and determination.",
-        "Suddenly, you realized that you are just",
-        "a normal person",
-        "who dreamed to be a Hero.",
-        "Ending 1: Dreamer"
+        "The monster started to chase you.",
+        "You tried to run away, but the monster was too fast.",
+        "The monster caught you and killed you.",
+        "Ending 1: Cold-Blooded Animal"
         }
     },
     {2, { //no villagers believe her
-        "no one believe",
+        "No one believed you.",
+        "The villagers thought you were crazy.",
+        "They took you to psychiatrist.",
+        "You were locked in a room...",
+        "...and never saw the sunlight again.",
+        "Ending 2: 'Three People Become Tigers'"
         }
     },
-    {3, { // not yet done
-        "He decided to befriend the monster.",
-        "He offered the monster some food and they became friends.",
-        "The monster protected him from other dangers.",
-        "He lived happily ever after.",
+    {3, { //all killed by hero and you were saved //Happy
+        "He rescued you from the Dragon.",
+        "You were so grateful to him and hugged him.",
+        "You two fell in love after that.",
+        "You two got married and lived happily ever after.",
+        "Is this the life you want?",
+        "Ending 3: Happy Ending?",
         }
     },
-    {4, {
+    {4, { //all killed by hero and you were saved //Sad
+        "He thought he rescued you from the Dragon.",
+        "Everyone said you should be grateful to him.",
+        "But only you know the truth...",
+        "But there isn't anyone you can talk with.",
+        "You can only hide the truth for the rest of the life.",
+        "Ending 4: The Untold Truth",
+        }
+    },
+    {5, { //true ending //incomplete
         "After searching for days, you still couldn't find her and went back to the village.",
         "But the villagers saw you talked to a monster happily.",
         "They think you betrayed them.",
@@ -47,15 +49,7 @@ unordered_map<int, vector<string> > Girl_endings = {
         "Or maybe, it's human.",
         "Biased, selfish, and ignorant.",
         "'Monster' that you can't defeat.",
-        "Ending 4: 'Monster'"
-        }
-    },
-    {5, {
-        "He decided to fight the monster.",
-        "He tried his best, but his attacks meant nothing to the monster.",
-        "The monster opened its mouth and swallowed him whole.",
-        "He was never seen again.",
-        "Ending 5: Why you try?"     
+        "Ending 5: 'Monster'"
         }
     },
 };
@@ -154,7 +148,7 @@ void Girl_run(Progress &progress, Player &player) {
                 if (!progress.finish_intro && progress.scn_num == 0) 
                     chat.loadChat("intro house", progress.map_code, progress.scn_num, player, font_cyan);
                     progress.finish_intro = 1;
-                    progress.scn_num = 1;
+                    progress.scn_num = 5;
 
             break;
 
@@ -187,11 +181,11 @@ void Girl_run(Progress &progress, Player &player) {
                     offsety = - 31;
                 }    
 
-                if (progress.first_time_entering_village && progress.scn_num == 1) 
+                if (progress.first_time_entering_village && progress.scn_num == 5) 
                 chat.loadChat("enter village", progress.map_code, progress.scn_num, player, font_cyan);
                 progress.first_time_entering_village = false;
 
-                if (progress.scn_num == 6) { // add hero doesnt know truth
+                if (progress.scn_num == 9) { 
                     chat.loadChat("no one believe", progress.map_code, progress.scn_num, player, font_red);
                     progress.event_num = 0;
                     progress.ending_num = 2;
@@ -237,118 +231,61 @@ void Girl_run(Progress &progress, Player &player) {
             case forest:
                 progress.map_code = 5;
 
-                if (progress.first_time_entering_forest && progress.scn_num == 1) 
-                chat.loadChat("enter forest", progress.map_code, progress.scn_num, player, font_cyan);
-                progress.first_time_entering_forest = false;
-
-                if (progress.can_enterMonster) {
-                    if (player.y >= 31) {
-                        map_state = monster;
-                        offsety = -30;
-                    }
-                }
-                else {
-                    if (player.y >= 31) {
-                        player.y = 30;
-                        if (progress.scn_num == 2) {
-                            chat.loadChat("no sword", progress.map_code, progress.scn_num, player, font_red);
-                        }
-                        else if (progress.scn_num == 6) {
-                            chat.loadChat("scare", progress.map_code, progress.scn_num, player, font_red);
-                        }
-                    }
-                } 
-
                 if (player.x <= 0) {
                     map_state = outside_village;
                     offsetx = 35;
                 }
 
-                if (progress.can_enterCastle) {
-                    if (player.x >= 35) {
+                if (player.y >= 31) {
+                        map_state = monster;
+                        offsety = -30;
+                }
+
+                if (player.x >= 35) {
                         map_state = castle;
                         offsetx = -36;
-                    }
                 }
-                else {
-                    if (player.x >= 35) {
-                        player.x = 34;
-                        if (progress.scn_num == 2) {
-                            chat.loadChat("no sword", progress.map_code, progress.scn_num, player, font_red);
-                        }
-                    }
-                } 
+
+                if (progress.first_time_entering_forest && progress.scn_num == 5) {
+                    chat.loadChat("enter forest", progress.map_code, progress.scn_num, player, font_cyan);
+                    progress.first_time_entering_forest = false;
+                }
 
             break;
 
             case monster:
-                progress.map_code = 6;
 
-                if (progress.scn_num == 1 && player.touch_monster) {
-                    progress.event_num = 1;
-                    window.handle_choice(progress, player);
-                    player.touch_monster = 0;
-                    map_state = castle;
-                    player.x = 18;
-                    player.y = 30;
-                }
-                
-                // if the player chose to leave the sword, he will be teleported back to the village (scare and escaped)
-                if (progress.scn_num == 4 && player.y >= 8) {
-                    progress.map_code = 0;
-                    player.x = 9;
-                    player.y = 9;
-                    progress.ending_num = 1;
-                    map_state = hero_house;
-                    player.reach_ending = true;
-                }
+                progress.map_code = 6;
 
                 if (player.y <= 0) {
                     map_state = forest;
                     offsety = 31;
                 }
 
-                if (progress.event_num == 3 && player.y >= 5) {
-                    window.handle_choice(progress, player);
-                    // if the player chose to escape, the player will be teleported back to the forest and cannot enter the monster's lair
-                    if (progress.scn_num == 6) {
-                        progress.can_enterMonster = false;
-                        progress.map_code = 5;
-                        player.x = 18;
-                        player.y = 30;
-                        map_state = forest;
-                    }
-                }
-
-                if (progress.event_num == 4 && player.touch_monster) {
-                    window.handle_choice(progress, player);
+                if (progress.scn_num == 5 && player.touch_monster) {
+                    progress.scn_num = 6;                    
+                    progress.event_num = 1;
                     player.touch_monster = 0;
+                    window.handle_choice(progress, player);
+                    if (progress.scn_num == 1) {
+                        progress.ending_num = 1;
+                        player.reach_ending = true;
+                    }
+                }   
+
+                if (progress.scn_num == 2) {
+                    progress.scn_num = 7;
+                    map_state = castle; 
+                    player.x = 18;
+                    player.y = 30;
                 }
 
-                if (progress.scn_num == 3 && player.y == 5 && !progress.saw_monster) {
-                    chat.loadChat("hero saw", progress.map_code, progress.scn_num, player, font_white);
-                    progress.saw_monster = 1;
-                }
-                if (progress.scn_num == 3 && player.y == 8 && !progress.monster_killed) {
-                    chat.loadChat("hero kill", progress.map_code, progress.scn_num, player, font_yellow);
-                    progress.monster_killed = 1;
-                }
+ 
 
             break;
             
             case castle:
                 progress.map_code = 7; 
-
-                if (progress.first_time_entering_castle && (progress.scn_num == 1 || progress.scn_num == 3)) {
-                    chat.loadChat("enter castle", progress.map_code, progress.scn_num, player, font_white);
-                    //lock the door collision
-                    if (player.touch_dragon) {
-                        chat.loadChat("talk to dragon", progress.map_code, progress.scn_num, player, font_white);
-                    }
-                    progress.first_time_entering_castle = false;
-                    progress.scn_num = 5;
-                }
-
 
                 if (player.y >= 31 && player.x >= 16 && player.x <= 19) {
                     map_state = back_village;
@@ -359,35 +296,34 @@ void Girl_run(Progress &progress, Player &player) {
                     offsetx = 36;
                 }
 
-                if (progress.first_time_entering_castle && (progress.scn_num == 1 || progress.scn_num == 6)) {
+                if (progress.first_time_entering_castle && (progress.scn_num == 5 || progress.scn_num == 7)) {
+                    progress.scn_num = 7;
                     chat.loadChat("enter castle", progress.map_code, progress.scn_num, player, font_white);
+                    //lock the door collision
                     progress.first_time_entering_castle = false;
+                    progress.scn_num = 8;
+                    //release door collision
                 }
 
-                if (progress.first_time_entering_castle && progress.scn_num == 3 && player.x == 5) {
-                    chat.loadChat("hero kill all", progress.map_code, progress.scn_num, player, font_red);
-                    progress.first_time_entering_castle = false;
+                if (!progress.talked_to_dragon && player.touch_dragon) {
+                        chat.loadChat("talk to dragon", progress.map_code, progress.scn_num, player, font_red);
+                        player.touch_dragon = 0;
+                        progress.talked_to_dragon = 1;
                 }
-
-                if (progress.event_num == 5 && player.touch_dragon) {
-                    window.handle_choice(progress, player);
-                    player.touch_dragon = 0;
-                }
-
+                
             break;
 
             case back_village:
                 progress.map_code = 8;
 
-                if (progress.first_time_entering_back_village && progress.scn_num == 5) { // add hero doesnt know truth
-                    chat.loadChat("back village", progress.map_code, progress.scn_num, player, font_red);
-                    progress.scn_num = 6;
-                }
-
-
                 if (player.x <= 0) {
                     map_state = oldman_house;
                     offsetx = 31;
+                }
+
+                if (progress.first_time_entering_back_village && progress.scn_num == 8) { // add hero doesnt know truth
+                    chat.loadChat("back village", progress.map_code, progress.scn_num, player, font_red);
+                    progress.scn_num = 9;
                 }
 
             break;
