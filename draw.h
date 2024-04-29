fe94e2774fd1248b2659929d8a701f5b3fcb9352
KY <162005150+Atar369@users.noreply.github.com>
2024-04-30 03:08:30 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -15,10 +15,6 @@ using namespace std;
 
 void draw_instance(int y, int x, int obj, string symbol);
 
-//bool next_map(vector<vector<string> > current_map, short &map_code);
-
 void draw_map(vector<vector<short> > &current_map, Player &player);
 
 void change_map(vector<vector<short> > & current_map, int target, int new_obj);
-
-//void change_after_touch(vector<vector<short> > &current_map, Player &player);
