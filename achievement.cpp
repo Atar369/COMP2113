2acfb51d3c06793a5b71581ae14cc3e6c45d211f
Kwokyw <160570241+Kwokyw@users.noreply.github.com>
2024-04-30 22:29:37 +0800

    Update achievement.cpp

--- achievement.cpp
+++ achievement.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main(){
-  bool ending1, ending2, ending3, ending4, ending5, ending6;
-  int i = 6;
-  for ( i = 0; i)
+  bool HeroEnding1, HeroEnding2, HeroEnding3, HeroEnding4, HeroEnding5, GirlEnding1, GirlEnding2, GirlEnding3, GirlEnding4;
+  int num = 6;
+  int i;
+  for ( i = 0; i < num; i++ )
+    cout << "Hero:" << endl;
+    cout << "Ending 1: Dreamer" << endl;
+    cout << "Ending 2: Killer" << endl;
+    cout << "Ending 3: Crazy" << endl;
+    cout << "Ending 4: 'Monster'" << endl;
+    cout << "Ending 5: Why you try?" << endl;
+  num = 5;
+  for ( i = 0; i < num; i++ )
+    cout << "Girl:" << endl;
+    cout << "Ending 1: Cold-Blooded Animal" << endl;
+    cout << "Ending 2: 'Three People Become Tigers'" << endl;
+    cout << "Ending 3: Happy Ending?" << endl;
+    cout << "Ending 4: The Untold Truth" << endl;
 }
