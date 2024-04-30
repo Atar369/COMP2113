7c830990f5626f4443d3455b88058cc8dd958689
KY <162005150+Atar369@users.noreply.github.com>
2024-04-30 20:39:01 +0800

    Update format.h

--- format.h
+++ format.h
@@ -18,8 +18,6 @@ using namespace std;
 #define italic "\033[3m"
 #define underline "\033[4m"
 #define blink "\033[5m"
-#define reverse "\033[7m"
-#define concealed "\033[8m"
 
 // Define font colours
 #define font_black "\033[30m"
@@ -31,21 +29,7 @@ using namespace std;
 #define font_cyan "\033[36m"
 #define font_white "\033[37m"
 
-// Define background colours
-#define back_black "\033[40m"
-#define back_red "\033[41m"
-#define back_green "\033[42m"
-#define back_yellow "\033[43m"
-#define back_blue "\033[44m"
-#define back_purple "\033[45m"
-#define back_cyan "\033[46m"
-#define back_white "\033[47m"
-
 // Define printing settings
-#define move_up_by_n "\033[nA"
-#define move_down_by_n "\033[nB"
-#define move_right_by_n "\033[nC"
-#define move_left_by_n "\033[nD"
 #define clear_screen "\033[2J"
 #define remove_whole_line "\033[K"
 #define save_cursor_position "\033[s"
@@ -60,8 +44,10 @@ extern unordered_map<short, string> effect_mapping;
 // Define effect pairs
 extern unordered_map<short, string> color_mapping;
 
-// Define ending color pairs of Hero
+// Define ending color pairs
 extern unordered_map<int, string> Hero_ending_color_mapping;
-
-// Define ending color pairs of Girl
 extern unordered_map<int, string> Girl_ending_color_mapping;
+
+
+
+
