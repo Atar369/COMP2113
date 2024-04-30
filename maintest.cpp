e2b072a80f87f3fbe0656a8a3438e11bf0584788
KY <162005150+Atar369@users.noreply.github.com>
2024-04-30 20:37:21 +0800

    Update maintest.cpp

--- maintest.cpp
+++ maintest.cpp
@@ -8,7 +8,6 @@ int main() {
     typedef enum {
         STATE_MENU,
         STATE_HTP,
-        STATE_ACHIEVE,
         STATE_GAME,
         STATE_CONTIN,
         STATE_EXIT,
@@ -24,16 +23,42 @@ int main() {
     int startRow = rows / 2;
     int startCol = cols / 2; // Adjust for the length of the longest string
     
-    // Init current state
+    // current state of the game
     game_states current_state;
     current_state = STATE_MENU;
     
+    // Progress, keyboard, window, player declaration
     Progress progress;
     Keyboard keyboard;
     Window window;
     Player player;
 
-    // Menu menu
+    // load progress
+    if (progress.checking_loading()) {
+        progress.load_progress(player);
+    }
+    
+    // if ending is 7, which is the true ending, don allow to play
+    if (progress.ending_num == 7) {
+        system("clear");
+        while (progress.ending_num == 7) {
+            cout << font_blue << dim;
+            draw_logo(rows, cols);
+            cout << reset_format;
+
+            string contents = "Determine your own destiny! We are living a happy life here. ----- By Hero";
+
+            cout << default_format << startRow - logo_h_size + 9 << ";" << startCol - (contents.length())/2 << "H" << contents << endl;
+
+            keyboard.get_userInput();
+            if (keyboard.key == KEY_EXIT || keyboard.key == KEY_ENTER || keyboard.key == KEY_SPACE) {
+                system("clear");
+                cout << show_cursor << endl; // Show cursor
+                return 0;
+            }
+        }
+    }
+    
     // Buttons
     const string button_start_game[2] = {
         "> NEW GAME <",
@@ -60,6 +85,7 @@ int main() {
         "  Exit  ",
     };
 
+    // content for how to play
     const string how_to_play[10] = {
         "How to Play",
         "W - Move Up                    E - Action       ",
@@ -75,12 +101,12 @@ int main() {
     
     static int menu_button = 0;
 
+    // Main loop
     while (current_state != STATE_EXIT) {
         // Clear screen
         system("clear");
         std::cout << save_cursor_position; // Save cursor position
         std::cout << hide_cursor;  
-        
 
        // Predine the variables
         int select_start_game = 1, select_HTP = 1, select_achieve = 1, select_exit = 1, select_continue = 1;
@@ -111,10 +137,10 @@ int main() {
                         menu_button++;
                     } 
 
-                    // if the player saved 
+                    // if the player saved, will have continue button
                     if (progress.checking_loading()) {
-                        if (menu_button >= 4) {
-                            menu_button = 4;
+                        if (menu_button >= 3) {
+                            menu_button = 3;
                         }
 
                         // check the button
@@ -122,35 +148,24 @@ int main() {
                             select_start_game = 0;
                             select_continue = 1;
                             select_HTP = 1;
-                            select_achieve = 1;
                             select_exit = 1;
                         } 
                         else if (menu_button == 1) {
                             select_start_game = 1;
                             select_continue = 0;
                             select_HTP = 1;
-                            select_achieve = 1;
                             select_exit = 1;
                         }
                         else if (menu_button == 2) {
                             select_start_game = 1;
                             select_continue = 1;
                             select_HTP = 0;
-                            select_achieve = 1;
                             select_exit = 1;
                         }
-                        else if (menu_button == 3) {
+                        else if (menu_button == 3){
                             select_start_game = 1;
                             select_continue = 1;
                             select_HTP = 1;
-                            select_achieve = 0;
-                            select_exit = 1;
-                        }
-                        else if (menu_button == 4){
-                            select_start_game = 1;
-                            select_continue = 1;
-                            select_HTP = 1;
-                            select_achieve = 1;
                             select_exit = 0;
                         }
 
@@ -161,9 +176,7 @@ int main() {
 
                         std::cout << default_format << startRow - logo_h_size + 13 << ";" << startCol - (button_HTP[select_HTP].length())/2 << "H" << button_HTP[select_HTP] << endl;
 
-                        std::cout << default_format << startRow - logo_h_size + 15 << ";" << startCol - (button_achieve[select_achieve].length())/2 << "H" << button_achieve[select_achieve] << endl;
-
-                        std::cout << default_format << startRow - logo_h_size + 17 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
+                        std::cout << default_format << startRow - logo_h_size + 15 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
 
                         // Click handler
                         switch(menu_button) {
@@ -177,52 +190,38 @@ int main() {
                                 current_state = STATE_HTP;
                                 break;
                             case 3:
-                                current_state = STATE_ACHIEVE;
-                                break;
-                            case 4:
                                 current_state = STATE_EXIT;
                                 break;    
                         }
                     }
 
                     else {
-                        if (menu_button >= 3) {
-                            menu_button = 3;
+                        if (menu_button >= 2) {
+                            menu_button = 2;
                         }
                         // check the button
                         if (menu_button == 0) {
                             select_start_game = 0;
                             select_HTP = 1;
-                            select_achieve = 1;
                             select_exit = 1;
                         } 
                         else if (menu_button == 1) {
                             select_start_game = 1;
                             select_HTP = 0;
-                            select_achieve = 1;
                             select_exit = 1;
                         }
                         else if (menu_button == 2) {
                             select_start_game = 1;
                             select_HTP = 1;
-                            select_achieve = 0;
-                            select_exit = 1;
-                        }
-                        else if (menu_button == 3){
-                            select_start_game = 1;
-                            select_HTP = 1;
-                            select_achieve = 1;
                             select_exit = 0;
                         }
 
-                        // Position the cursor and print the text at the center
+                        // Position the cursor and print the text at the center, without continue button
                         std::cout << default_format << startRow - logo_h_size + 9 << ";" << startCol - (button_start_game[select_start_game].length())/2 << "H" << button_start_game[select_start_game] << endl;
 
                         std::cout << default_format << startRow - logo_h_size + 11 << ";" << startCol - (button_HTP[select_HTP].length())/2 << "H" << button_HTP[select_HTP] << endl;
 
-                        std::cout << default_format << startRow - logo_h_size + 13 << ";" << startCol - (button_achieve[select_achieve].length())/2 << "H" << button_achieve[select_achieve] << endl;
-
-                        std::cout << default_format << startRow - logo_h_size + 15 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
+                        std::cout << default_format << startRow - logo_h_size + 13 << ";" << startCol - (button_exit[select_exit].length())/2 << "H" << button_exit[select_exit] << endl;
 
                         // Click handler
                     switch(menu_button) {
@@ -233,9 +232,6 @@ int main() {
                             current_state = STATE_HTP;
                             break;
                         case 2:
-                            current_state = STATE_ACHIEVE;
-                            break;
-                        case 3:
                             current_state = STATE_EXIT;
                             break;    
                     }
@@ -244,9 +240,10 @@ int main() {
                     keyboard.get_userInput();
                 }
 
+                
                 break;
 
-            // Info
+            // how to play
             case STATE_HTP:
     
                 // How to play
@@ -262,26 +259,18 @@ int main() {
                 
                 break;
 
-            case STATE_ACHIEVE:
-                std::cout << "Achievements" << endl;
-                // to be implemented
-                keyboard.get_userInput();
-
-                // Exit to menu
-                if (keyboard.key == KEY_EXIT)
-                    current_state = STATE_MENU;
-                break;
-
            // Game
             case STATE_GAME:
+
                 // Clear previous progress
                 progress.delete_progress();
-                player.reach_ending = 0;
                 window.is_Hero = false;
                 window.is_Girl = false;
+                player.reset_player();
                 window.intro_character_choice(player);
-                
+
                 if (window.is_Hero) {
+                    // Set player as hero
                     player.x = 9;
                     player.y = 9;
                     player.symbol = "|@|";
@@ -289,10 +278,12 @@ int main() {
                     progress.map_code = 0;
                     progress.event_num = 1;
                     progress.ending_num = 0;
+
                     // Start game
                     Hero_run(progress, player);
                 }
                 else if (window.is_Girl) {
+                    // set player as girl
                     player.x = 9;
                     player.y = 9;
                     player.symbol = "|%|";
@@ -300,16 +291,39 @@ int main() {
                     progress.map_code = 1;
                     progress.event_num = 0;
                     progress.ending_num = 0;
+
                     // Start game
                     Girl_run(progress, player);
                 }    
 
+                // Ending 7 is the true ending, then the game will end
+                if (progress.ending_num == 7) {
+                    system("clear");
+                    progress.save_progress(player);
+                    while (progress.ending_num == 7) {
+                        cout << font_blue << dim;
+                        draw_logo(rows, cols);
+                        cout << reset_format;
 
+                        string contents = "Determine your own destiny! We are living a happy life here. ----- By Hero";
+
+                        cout << default_format << startRow - logo_h_size + 9 << ";" << startCol - (contents.length())/2 << "H" << contents << endl;
+
+                        keyboard.get_userInput();
+                        if (keyboard.key == KEY_EXIT || keyboard.key == KEY_ENTER || keyboard.key == KEY_SPACE) {
+                            system("clear");
+                            cout << show_cursor << endl; // Show cursor
+                            return 0;
+                        }
+                    }
+                }
+
+                // player can choose to save or not if not yet reach the ending
                 if (!player.reach_ending) {
                     window.handle_save_choice(choice_button, progress.map_code, player);
                 }
-                else {
-                    // reset progress after reaching the ending
+                else if (progress.ending_num != 7){
+                    // reset progress after reaching the ending, return to menu directly without choice
                     progress.reset_progress();
                     if (player.color == font_blue) {
                         player.x = 9;
@@ -336,11 +350,14 @@ int main() {
                 break;
 
             case STATE_CONTIN:
-                
+                // Load progress
                 progress.load_progress(player);
                 
+                // Check if the player has reached the ending
                 if (player.reach_ending) {
                     progress.reset_progress();
+                    window.is_Girl = false;
+                    window.is_Hero = false;
                     window.intro_character_choice(player);
                     if (player.color == font_blue) {
                         player.x = 9;
@@ -353,7 +370,7 @@ int main() {
                         progress.event_num = 0;
                         progress.map_code = 1;
                     }  
-                    player.reach_ending = 0;
+                    player.reset_player();
                 }
                 
                 if (player.color == font_blue) {
@@ -369,15 +386,40 @@ int main() {
                     Girl_run(progress, player);
                 }
 
+                // Ending 7 is the true ending, then the game will end
+                if (progress.ending_num == 7) {
+                    system("clear");
+                    progress.save_progress(player);
+                    while (progress.ending_num == 7) {
+                        cout << font_blue << dim;
+                        draw_logo(rows, cols);
+                        cout << reset_format;
+
+                        string contents = "Determine your own destiny! We are living a happy life here. ----- By Hero";
+
+                        cout << default_format << startRow - logo_h_size + 9 << ";" << startCol - (contents.length())/2 << "H" << contents << endl;
+
+                        keyboard.get_userInput();
+                        if (keyboard.key == KEY_EXIT || keyboard.key == KEY_ENTER || keyboard.key == KEY_SPACE) {
+                            system("clear");
+                            cout << show_cursor << endl; // Show cursor
+                            return 0;
+                        }
+                    }
+                }
+
+                // player can choose to save or not if not yet reach the ending
                 if (!player.reach_ending) {
                     window.handle_save_choice(choice_button, progress.map_code, player);
                 }
-                else {
-                    // reset progress after reaching the ending
+                else if (progress.ending_num != 7) {
+                    // reset progress after reaching the ending and return to menu directly without choice
                     progress.reset_progress();  
                     progress.save_progress(player);
+                        
                     current_state = STATE_MENU; 
                 }
+                
 
                 if (choice_button == 0)
                     // save progress
@@ -389,15 +431,14 @@ int main() {
                 break;
 
             case STATE_EXIT:
-                std::cout << "Goodbye!" << endl;
                 break;
-                }
+            }
  
         }
     
-    system("clear"); // Clear screen
-    std::cout << restore_cursor_position; // Restore cursor position
-    std::cout << show_cursor; // Show cursor
+    
+    system("clear");
+    std::cout << show_cursor << endl; // Show cursor
     return 0;
     
 }
