9b2803fad5bade820b46ec959015f3f77d366bc0
KristyLWY <162736551+KristyLWY@users.noreply.github.com>
2024-04-30 20:12:03 +0800

    Update format.cpp

--- format.cpp
+++ format.cpp
@@ -1,27 +1,6 @@
 #include "format.h"
 
-/*
-unordered_map<string, string> effect_string_map = {
-    {"reset", reset}, 
-    {"dim", dim}, 
-    {"italic", italic}, 
-    {"bold", bold}, 
-    {"underline", underline}, 
-    {"blink", blink},
-};
-
-unordered_map<string, string> font_string_map = {
-    {"font_black", font_black}, 
-    {"font_red", font_red}, 
-    {"font_green", font_green}, 
-    {"font_yellow", font_yellow}, 
-    {"font_blue", font_blue}, 
-    {"font_purple", font_purple}, 
-    {"font_cyan", font_cyan}, 
-    {"font_white", font_white}
-};*/
-
-// Define color pairs 
+// Make effect for some particular objects
 unordered_map<short, string> effect_mapping = {
     {c_grass      ,   italic},
     {c_dragon     ,   bold},
@@ -132,6 +111,7 @@ unordered_map<short, string> color_mapping = {
     {c_mysterygirl, font_purple}
 };
 
+// Set the colour of Hero ending
 unordered_map<int, string> Hero_ending_color_mapping = {
     {1, font_green},
     {2, font_red},
@@ -142,11 +122,11 @@ unordered_map<int, string> Hero_ending_color_mapping = {
     {7, font_white} // true ending
 };
 
+// Set the colour of Girl ending
 unordered_map<int, string> Girl_ending_color_mapping = {
     {1, font_green},
     {2, font_red},
     {3, font_cyan},
     {4, font_blue},
-    {5, font_red},
-    {6, font_purple}, // hint voice, not ending
+    {5, font_purple}, // hint voice, not ending
 };
